d6d56f81a130f5c11909a6ad1c1a7c9f91a388a6
Michele Cipriani <michele.cipriani.unipegaso@gmail.com>
2025-07-04 11:22:44 +0200

    commit 04/07 mattina

--- Lezione10_04-07-2025/EsercizioInterfaccia1/ConseganaManager.java
+++ Lezione10_04-07-2025/EsercizioInterfaccia1/ConseganaManager.java
@@ -21,7 +21,48 @@ public class ConseganaManager {
         int scelta;
 
         do {
-            
+            //menù
+            System.out.println("===MENU'===");
+            System.out.println("1 - Aggiungi veicolo");
+            System.out.println("2 - Chiedi Consegna");
+            System.out.println("3 - Traccia Consegna");
+            System.out.println("4 - Esci");
+            //valorizzo la scelta
+            scelta = myScannerInt.nextInt();
+            //switch per i casi del menù
+            switch (scelta) {
+                case 1:
+                    System.out.println("Inserisci targa: ");
+                    //valorizzo l'input'
+                    String targa = myScannerStr.nextLine();
+                    System.out.println("Inserisci carico massimo: ");
+                    //valorizzo l'input'
+                    float carico = myScannerInt.nextInt();
+                    //richiedo il tipo di veicolo
+                    System.out.println("Tipo di veicolo? (Furgone o Drone) ");
+                    //valorizzo l'input'
+                    String risposta = myScannerStr.nextLine().toLowerCase();
+                    //se è drone
+                    if (risposta.equals("drone")) {
+                        Drone drone = new Drone(targa, carico);
+                        flottaConsegne.add(drone);
+                        System.out.println("Drone registrato correttamente!");
+                    //se è furgone
+                    } else if (risposta.equals("furgone")){
+                        Furgone furgone = new Furgone(targa, carico);
+                        flottaConsegne.add(furgone);
+                        System.out.println("Furgone registrato correttamente!");
+                    }
+                    break;
+                case 2:
+                    //richiedo il tipo di veicolo
+                    System.out.println("Tipo di veicolo? (Furgone o Drone) ");
+                    //valorizzo l'input'
+                    String veicolo = myScannerStr.nextLine().toLowerCase();
+                    
+                default: System.out.println("Scelta errata!");
+                    break;
+            }
             
         } while (scelta != 4);
 
